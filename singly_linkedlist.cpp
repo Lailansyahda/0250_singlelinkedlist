f41da68e08e8c55c605f553c2033db6d7d4c6081
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-06 11:30:16 +0700

    if

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -4,30 +4,43 @@ using namespace std;
 
 class Node
 {
-    public :
+public:
     int noMhs;
     Node *next;
 };
 
 class LinkedList
 {
-    Node *START;
-
-    public:
+   Node *START;
+   
+public:
     LinkedList()
     {
         START = NULL;
     }
-}
 
-void addNode()
+    void addNode()
     {
         int nim;
-        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cout << "\nMasukan Nomor Mahasiswa: ";
         cin >> nim;
-        
+
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
 
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
 
+        Node *previous = START;
+        Node *current = START;
 
+        
