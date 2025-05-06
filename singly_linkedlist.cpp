4443665ea66f7e704a843f784a269bca0e10a58d
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-06 11:06:30 +0700

    new node

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -25,5 +25,9 @@ void addNode()
         int nim;
         cout << "\nMasukkan Nomor Mahasiswa: ";
         cin >> nim;
+        
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
 
 
